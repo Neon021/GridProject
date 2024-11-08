3edffde25dd74e8fddf17952d4acc1954915607d
Adrian Yigit <bilalyigit594@gmail.com>
2024-11-08 16:21:48 +0300

    Need to switch to main PC, I don't remember the corresponding textures to CSV codes

--- Game1.cs
+++ Game1.cs
@@ -12,7 +12,8 @@ namespace GridProject
     {
         private readonly string TileTexture = "TileTexture";
         private readonly string CharacterTexture = "Character";
-        private readonly string TextureAtlas = "GrassTiles";
+        //private readonly string TextureAtlas = "GrassTiles";
+        private readonly string TextureAtlas = "minimal_assets";
         private int ScreenHeight;
         private int ScreenWidth;
 
@@ -20,26 +21,38 @@ namespace GridProject
         private SpriteBatch _spriteBatch;
         private ContentManager _contentManager;
 
-        private readonly Dictionary<Vector2, int> TileMap = new();
-        private List<Rectangle> _textureStore;
+        //private readonly Dictionary<Vector2, int> TileMap = new();
+        private Dictionary<Vector2, int> mg = new();
+        private Dictionary<Vector2, int> fg = new();
+        private Dictionary<Vector2, int> colorLayer = new();
+        private Dictionary<Vector2, int> collisions = new();
         private Texture2D _textureAtlas;
+        private Dictionary<int, Rectangle> _textureStore;
+        //private List<Rectangle> _textureStore;
+        //private Texture2D _textureAtlas;
 
-        private Texture2D _tileTexture;
-        private int _tileHeight;
-        private int _tileWidth;
+        //private Texture2D _tileTexture;
+        //private int _tileHeight;
+        //private int _tileWidth;
 
-        private MovingSprite _movingSprite;
+        //private MovingSprite _movingSprite;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            TileMap = LoadTileMap("Map.csv");
             IsMouseVisible = true;
+            //_textureStore = new()
+            //{
+            //    new(23, 103, 47, 17),
+            //    new(7, 15, 69, 79)
+            //};
             _textureStore = new()
             {
-                new(23, 103, 47, 17),
-                new(7, 15, 69, 79)
+                //Determine which textures were used from Tiled file
+                //Calculate their positions and width/height from the texture atlas
+                //And use thie CSV number as their Key part 
+
             };
         }
 
@@ -79,14 +92,18 @@ namespace GridProject
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
-            _tileTexture = _contentManager.Load<Texture2D>(TileTexture);
-            _tileHeight = _tileTexture.Height;
-            _tileWidth = _tileTexture.Width;
+            //_tileTexture = _contentManager.Load<Texture2D>(TileTexture);
+            //_tileHeight = _tileTexture.Height;
+            //_tileWidth = _tileTexture.Width;
 
-            _movingSprite = new(_contentManager.Load<Texture2D>(CharacterTexture), Vector2.Zero, 5f);
+            //_movingSprite = new(_contentManager.Load<Texture2D>(CharacterTexture), Vector2.Zero, 5f);
 
-            _textureAtlas = _contentManager.Load<Texture2D>(TextureAtlas);
+            //_textureAtlas = _contentManager.Load<Texture2D>(TextureAtlas);
+
+            mg = LoadTileMap("Data/level1_mg.csv");
+            fg = LoadTileMap("Data/level1_fg.csv");
+            colorLayer = LoadTileMap("Data/level1_color layer.csv");
+            collisions = LoadTileMap("Data/level1_collisions.csv");
         }
 
         protected override void Update(GameTime gameTime)
@@ -94,8 +111,8 @@ namespace GridProject
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-            _movingSprite.Update();
+            //_movingSprite.Update();
+
             base.Update(gameTime);
         }
 
@@ -104,10 +121,9 @@ namespace GridProject
             GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
-            int numTilesX = (int)Math.Ceiling((float)ScreenWidth / _tileWidth);
-            int numTilesY = (int)Math.Ceiling((float)ScreenHeight / _tileHeight);
+            //int numTilesX = (int)Math.Ceiling((float)ScreenWidth / _tileWidth);
+            //int numTilesY = (int)Math.Ceiling((float)ScreenHeight / _tileHeight);
 
-            _spriteBatch.Begin();
 
             //for (int x = 0; x < numTilesX; x++)
             //{
@@ -119,18 +135,19 @@ namespace GridProject
 
             //_spriteBatch.Draw(_movingSprite.Texture, _movingSprite.Rectangle, Color.White);
 
-            foreach (var kv in TileMap)
-            {
-                Rectangle dest = new(
-                    (int)kv.Key.X * 128,
-                    (int)kv.Key.Y * 128,
-                    128,
-                    128);
+            //foreach (var kv in TileMap)
+            //{
+            //    Rectangle dest = new(
+            //        (int)kv.Key.X * 128,
+            //        (int)kv.Key.Y * 128,
+            //        128,
+            //        128);
 
-                Rectangle src = _textureStore[kv.Value - 1];
+            //    Rectangle src = _textureStore[kv.Value - 1];
 
-                _spriteBatch.Draw(_textureAtlas, dest, src, Color.White);
-            }
+            //    _spriteBatch.Draw(_textureAtlas, dest, src, Color.White);
+            //}
+            _spriteBatch.Begin();
 
             _spriteBatch.End();
 
