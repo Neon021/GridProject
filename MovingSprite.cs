5a6396a46230a0273e810f2231365eba3b94eb0e
Furkan Bilal Yiğit <bilalyigit594@gmail.com>
2024-11-12 15:00:00 +0300

    [CHANGE] Sprite class tweaked for the new structure

--- MovingSprite.cs
+++ MovingSprite.cs
@@ -4,34 +4,34 @@ using Microsoft.Xna.Framework.Input;
 
 namespace GridProject
 {
-    public class MovingSprite : ScaledSprite
-    {
-        private float Speed;
-        private bool _isMoving;
-        public MovingSprite(Texture2D texture, Vector2 position, float speed) : base(texture, position)
-        {
-            Speed = speed;
-        }
+    //public class MovingSprite : ScaledSprite
+    //{
+    //    private float Speed;
+    //    private bool _isMoving;
+    //    public MovingSprite(Texture2D texture, Vector2 position, float speed) : base(texture, position)
+    //    {
+    //        Speed = speed;
+    //    }
 
-        public override void Update()
-        {
-            if (Keyboard.GetState().IsKeyDown(Keys.Up))
-            {
-                Position.Y -= Speed;
-            }
-            else if (Keyboard.GetState().IsKeyDown(Keys.Down))
-            {
-                Position.Y += Speed;
-            }
-            else if (Keyboard.GetState().IsKeyDown(Keys.Right))
-            {
-                Position.X += Speed;
-            }
-            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
-            {
-                Position.X -= Speed;
-            }
-            base.Update();
-        }
-    }
+        //public override void Update()
+        //{
+        //    if (Keyboard.GetState().IsKeyDown(Keys.Up))
+        //    {
+        //        Position.Y -= Speed;
+        //    }
+        //    else if (Keyboard.GetState().IsKeyDown(Keys.Down))
+        //    {
+        //        Position.Y += Speed;
+        //    }
+        //    else if (Keyboard.GetState().IsKeyDown(Keys.Right))
+        //    {
+        //        Position.X += Speed;
+        //    }
+        //    else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+        //    {
+        //        Position.X -= Speed;
+        //    }
+        //    base.Update();
+        //}
+    //}
 }
--- ScaledSprite.cs
+++ ScaledSprite.cs
@@ -3,17 +3,17 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace GridProject
 {
-    public class ScaledSprite : Sprite
-    {
-        public Rectangle Rectangle
-        {
-            get
-            {
-                return new((int)Position.X, (int)Position.Y, 150, 200);
-            }
-        }
-        public ScaledSprite(Texture2D texture, Vector2 position) : base(texture, position)
-        {
-        }
-    }
+    //public class ScaledSprite : Sprite
+    //{
+    //    public Rectangle Rectangle
+    //    {
+    //        get
+    //        {
+    //            return new((int)Position.X, (int)Position.Y, 150, 200);
+    //        }
+    //    }
+    //    public ScaledSprite(Texture2D texture, Vector2 position) : base(texture, position)
+    //    {
+    //    }
+    //}
 }
