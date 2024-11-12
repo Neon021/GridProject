779ba8eb302a317fa48af2bcd3380909bda293fa
Furkan Bilal Yiğit <bilalyigit594@gmail.com>
2024-11-12 13:23:16 +0300

    [CHANGE] Old code removed and new exports added

--- Game1.cs
+++ Game1.cs
@@ -10,9 +10,6 @@ namespace GridProject
 {
     public class Game1 : Game
     {
-        private readonly string TileTexture = "TileTexture";
-        private readonly string CharacterTexture = "Character";
-        private readonly string TextureAtlas = "GrassTiles";
         private int ScreenHeight;
         private int ScreenWidth;
 
@@ -20,27 +17,18 @@ namespace GridProject
         private SpriteBatch _spriteBatch;
         private ContentManager _contentManager;
 
-        private readonly Dictionary<Vector2, int> TileMap = new();
-        private List<Rectangle> _textureStore;
+        private readonly Dictionary<Vector2, int> fg;
+        private readonly Dictionary<Vector2, int> mg;
+        private readonly Dictionary<Vector2, int> collisions;
         private Texture2D _textureAtlas;
-
-        private Texture2D _tileTexture;
-        private int _tileHeight;
-        private int _tileWidth;
-
-        private MovingSprite _movingSprite;
-
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            TileMap = LoadTileMap("Map.csv");
+            fg = LoadTileMap("Data/level1_fg.csv");
+            mg = LoadTileMap("Data/level1_mg.csv");
+            collisions = LoadTileMap("Data/level1_collisions.csv");
             IsMouseVisible = true;
-            _textureStore = new()
-            {
-                new(23, 103, 47, 17),
-                new(7, 15, 69, 79)
-            };
         }
 
         private static Dictionary<Vector2, int> LoadTileMap(string fileName)
@@ -55,7 +43,7 @@ namespace GridProject
                 string[] values = line.Split(',');
                 for (int x = 0; x < values.Length; x++)
                 {
-                    if (values[x] != "0")
+                    if (values[x] != "-1")
                         res.Add(new(x, y), int.Parse(values[x]));
                 }
 
@@ -79,14 +67,7 @@ namespace GridProject
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            // TODO: use this.Content to load your game content here
-            _tileTexture = _contentManager.Load<Texture2D>(TileTexture);
-            _tileHeight = _tileTexture.Height;
-            _tileWidth = _tileTexture.Width;
-
-            _movingSprite = new(_contentManager.Load<Texture2D>(CharacterTexture), Vector2.Zero, 5f);
-
-            _textureAtlas = _contentManager.Load<Texture2D>(TextureAtlas);
+            _textureAtlas = Content.Load<Texture2D>("Textures-16");
         }
 
         protected override void Update(GameTime gameTime)
@@ -94,8 +75,6 @@ namespace GridProject
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
-            _movingSprite.Update();
             base.Update(gameTime);
         }
 
@@ -103,35 +82,7 @@ namespace GridProject
         {
             GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
-            int numTilesX = (int)Math.Ceiling((float)ScreenWidth / _tileWidth);
-            int numTilesY = (int)Math.Ceiling((float)ScreenHeight / _tileHeight);
-
             _spriteBatch.Begin();
-
-            //for (int x = 0; x < numTilesX; x++)
-            //{
-            //    for (int y = 0; y < numTilesY; y++)
-            //    {
-            //        _spriteBatch.Draw(_tileTexture, new Vector2(x * _tileWidth, y * _tileHeight), Color.White);
-            //    }
-            //}
-
-            //_spriteBatch.Draw(_movingSprite.Texture, _movingSprite.Rectangle, Color.White);
-
-            foreach (var kv in TileMap)
-            {
-                Rectangle dest = new(
-                    (int)kv.Key.X * 128,
-                    (int)kv.Key.Y * 128,
-                    128,
-                    128);
-
-                Rectangle src = _textureStore[kv.Value - 1];
-
-                _spriteBatch.Draw(_textureAtlas, dest, src, Color.White);
-            }
-
             _spriteBatch.End();
 
             base.Draw(gameTime);
