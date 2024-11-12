22a29b1417ffa723756c8bc6d273e2b93d47c247
Furkan Bilal Yiğit <bilalyigit594@gmail.com>
2024-11-12 14:01:57 +0300

    [ADD] Tile map drawing abstracted in a new class

---
+++ PixelTile.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace GridProject
+{
+    public class PixelTile : Tile
+    {
+        public PixelTile(Dictionary<Vector2, int> tileMap) : base(tileMap)
+        {
+            displaySize = 28;
+            tilePerRow = 64;
+            pixelTileSize = 8;
+
+            TileMap = tileMap;
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, Texture2D textureAtlas)
+        {
+            foreach (var item in TileMap)
+            {
+                Rectangle dRect = new(
+                    (int)item.Key.X * displaySize,
+                    (int)item.Key.Y * displaySize,
+                    displaySize,
+                    displaySize
+                );
+
+                //Because the Value in KV is the actual ID of the tile in the atlas
+                int x = item.Value % tilePerRow;
+                int y = item.Value / tilePerRow;
+
+                Rectangle src = new(
+                    x * pixelTileSize,
+                    y * pixelTileSize,
+                    pixelTileSize,
+                pixelTileSize
+                );
+
+                spriteBatch.Draw(textureAtlas, dRect, src, Color.White);
+            }
+        }
+    }
+}
---
+++ Tile.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Collections.Generic;
+
+namespace GridProject
+{
+    public abstract class Tile
+    {
+        public int displaySize;
+        public int tilePerRow;
+        public int pixelTileSize;
+        public Dictionary<Vector2, int> TileMap;
+
+        public Tile(Dictionary<Vector2, int> tileMap)
+        {
+            TileMap = tileMap;
+        }
+
+
+        public virtual void Draw(SpriteBatch spriteBatch, Texture2D textureAtlas)
+        {
+
+        }
+    }
+}
